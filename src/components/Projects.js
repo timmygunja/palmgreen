f4307603bf521ca4ae80e976897bf851046255dc
timmygunja <t.urusov13@mail.ru>
2023-02-25 17:19:00 +0300

    Tweaked projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -31,7 +31,48 @@ const Projects = (props) => {
   //   }
   // }, []);
 
-  return <div className={"projects"}></div>;
+  return (
+    <div className={"projects"}>
+      <a
+        className="project-card"
+        href="https://mern-timmygunja.vercel.app"
+        target="_blank"
+      >
+        <div className="project-card-prev hard-centered">
+          <div>
+            <div className="project-card-logo">
+              <img src={"../../layers.png"}></img>
+            </div>
+            <p>mern-learn</p>
+          </div>
+        </div>
+        <div className="centered-vertically">
+          <div className="project-card-info">
+            <p>Simple Fullstack E-Commerce Website Application</p>
+            <p>MongoDB Express React NodeJS</p>
+            <p>
+              <a
+                href="https://github.com/timmygunja/mern-learn-frontend"
+                target="_blank"
+                className="project-link"
+              >
+                Frontend: 24 folders / 82 files / 4442 code lines
+              </a>
+            </p>
+            <p>
+              <a
+                href="https://github.com/timmygunja/mern-learn-backend"
+                target="_blank"
+                className="project-link"
+              >
+                Backend: 7 folders / 17 files / 934 code lines
+              </a>
+            </p>
+          </div>
+        </div>
+      </a>
+    </div>
+  );
 };
 
 export default Projects;
